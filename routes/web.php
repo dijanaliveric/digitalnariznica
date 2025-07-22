f6605ac9719736e2a36ba1bee4e3cc5a5f8b3bf4
dijanaliveric <dijanaliveric@gmail.com>
2025-07-22 19:23:15 +0200

    Dodana home view i rute

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', function () {
+    return view('home');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
