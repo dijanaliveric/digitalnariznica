3d52f6cf6254442b163b22cd4e77fb3bfac325c4
dijanaliveric <dijanaliveric@gmail.com>
2025-07-23 13:07:00 +0200

    Add clean Vite/Tailwind build to docs and update config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // kad builda, svi linkovi u index.html bit će relativni
   base: './',
   build: {
-    outDir: 'docs',      // generiraj docs/ folder
-    assetsDir: 'assets', // u njemu će biti CSS/JS
-    emptyOutDir: true    // obriši docs/ prije svakog builda
+    outDir: 'docs',
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 });
