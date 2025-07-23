035b01d2a4628574acfe43c0108c57b1e18d38a9
dijanaliveric <dijanaliveric@gmail.com>
2025-07-23 10:55:04 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/', function () {
-    return view('home');
-});
+
 
 
 Route::middleware('auth')->group(function () {
