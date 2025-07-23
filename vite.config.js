d9b592e4b9edc88d3d16fe9fe532f372f9667a1b
dijanaliveric <dijanaliveric@gmail.com>
2025-07-23 13:24:31 +0200

    Revert vite.config.js to original Laravel setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'docs',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  }
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
